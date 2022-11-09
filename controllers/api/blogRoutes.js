90bfa740853e6efe14d07d103053944ae7eb5058
Darin VanAusdal <darinv@iMac2017.local>
2022-11-09 03:38:28 -0700

    post routes work

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -4,13 +4,13 @@ const {Blog, User} = require('../../models');
 router.post('/', async (req, res) => {
     try {
         console.log(req.body);
-        // Add the date to the calendar / dashboard
+        
         req.body.user_id = req.session.user_id;
-        const newWorkout = await Workout.create(req.body);
+        const newBlog = await Blog.create(req.body);
         req.session.save(() => {
-            req.session.user_id = newWorkout.user_id;
+            req.session.user_id = newBlog.user_id;
             req.session.logged_in = true;
-            res.status(200).json(newWorkout);
+            res.status(200).json(newBlog);
         });
     } catch (err) {
         console.log(err);
